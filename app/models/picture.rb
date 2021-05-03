213126a58a4cc18c2d8a3771cd0b20c91c3e073e
teramoto-chihiro <komorikiri1031@gmail.com>
2021-05-03 08:57:40 +0000

    お気に入り機能作成

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   validates :content, presence: true
   validates :image, presence: true
   mount_uploader :image, ImageUploader
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  has_many :pictures
+  has_many :favorites, dependent: :destroy
   mount_uploader :image, ImageUploader
   before_validation { email.downcase! }
-    validates :name, presence: true, length: { maximum: 30 }
-    validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
-    has_many :pictures
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
